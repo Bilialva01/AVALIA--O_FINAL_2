d83d0a1d265c6c87e860c81e6e362661b9c49e0c
Pietro <pietrobilialva@hotmail.com>
2022-10-23 22:09:55 -0300

    first commit

---
+++ AVALIA--O-FINAL---MODULO-2-master/Js/recados.js
@@ -0,0 +1,112 @@
+let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+
+let logado = document.querySelector('#logado')
+
+
+
+logado.innerHTML = `Olá ${userLogado.nome}`
+
+if(localStorage.getItem('token') == null) {
+  alert('Você precisa estar logado para acessar essa página')
+  location.href = './Index.html'
+}
+
+function sair(){
+  localStorage.removeItem('token')
+  localStorage.removeItem('userLogado')
+  location.href = './Index.html'
+}
+
+let deveEditar = false;
+let indiceEdicao = 0;
+
+const form = document.querySelector("#infos_recados");
+const corpoRecados = document.querySelector("#tbody");
+const recuperarLocalStorage = () => {
+  const recados = JSON.parse(localStorage.getItem(userLogado.user) || "[]");
+  return recados;
+};
+
+const salvarLembrete = (event) => {
+  event.preventDefault();
+  const lembrete = form.lembrete.value;
+  const detalhamento = form.detalhamento.value;
+  const recados = recuperarLocalStorage();
+  if (deveEditar === true) {
+    alert("Editando recado");
+    recados[indiceEdicao].lembrete = lembrete;
+    recados[indiceEdicao].detalhamento = detalhamento;
+    deveEditar = false;
+  } else {
+    recados.push({
+      id: definirID(),
+      lembrete,
+      detalhamento,
+    });
+
+    alert("Lembrete adicionado com sucesso!");
+  }
+  localStorage.setItem(userLogado.user, JSON.stringify(recados));
+  form.lembrete.value = "";
+  form.detalhamento.value = "";
+  preencherTabela();
+};
+
+const preencherTabela = () => {
+  const recados = recuperarLocalStorage();
+  corpoRecados.innerHTML = "";
+  for (const recado of recados) {
+    corpoRecados.innerHTML += `
+<tr>
+     
+     <td> ${recado.id}</td>
+     <td> ${recado.lembrete}</td>
+     <td> ${recado.detalhamento}</td>
+     <td> 
+     <img src="../img/delet.svg" alt="lixeira" class="imgs" onclick="apagarRecado(${recado.id})" />
+     <img src="../img/edit.svg" alt="editor" class="imgs" onclick="editarRecado(${recado.id})"/>
+     </td>
+</tr>
+     `;
+  }
+};
+
+const apagarRecado = (id) => {
+  const recados = recuperarLocalStorage();
+
+  const indice = recados.findIndex((rec) => {
+    return rec.id === id;
+  });
+
+  recados.splice(indice, 1);
+  localStorage.setItem(userLogado.user, JSON.stringify(recados));
+  preencherTabela();
+};
+
+const editarRecado = (id) => {
+  const recados = recuperarLocalStorage();
+  const indice = recados.findIndex((rec) => rec.id === id);
+  const recado = recados[indice];
+  form.lembrete.value = recado.lembrete;
+  form.detalhamento.value = recado.detalhamento;
+  deveEditar = true;
+  indiceEdicao = indice;
+};
+
+const definirID = () => {
+  let max = 0;
+  const recados = recuperarLocalStorage();
+  recados.forEach((recado) => {
+    if (recado.id > max) max = recado.id;
+  });
+  return max + 1;
+};
+
+if (localStorage.getItem("userLogado") == null) {
+  alert("Você precisa estar logado para acessar esta página");
+  document.location.href = "./index.html";
+}
+
+
+form.addEventListener("submit", salvarLembrete);
+document.addEventListener("DOMContentLoaded", preencherTabela);
